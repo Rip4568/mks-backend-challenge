23610ceff109167fe5e124e11f4ebcfc9689fa0f
Jonathas David Santos do Nascimento <jhone.test14@gmail.com>
2024-04-08 15:25:27 -0300

    adiciona configuração para o typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+/* import { DataSource } from 'typeorm';
+import { User } from './users/entities/user.entity'; */
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    MoviesModule,
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      synchronize: true,
+      retryAttempts: 10000,
+      autoLoadEntities: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }), //ou pode especifcar no entities todos os objetos de entidades que serão usados um por um eg: Users, Movis...
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
